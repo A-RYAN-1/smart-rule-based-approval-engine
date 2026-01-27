1eaaaefee7a94ff231acef08bac6ff2ec80c7758
Ankita-Advitot <ankita.advitot@joshsoftware.com>
2026-01-27 12:03:38 +0530

    Added admin  migration file

--- migrations/fix_balances_and_seed.sql
+++ migrations/fix_balances_and_seed.sql
@@ -3,21 +3,21 @@ BEGIN;
 INSERT INTO leaves (user_id, total_allocated, remaining_count)
 VALUES
   (2, 36, 36),
-  (3, 45, 45)
+  (1, 45, 45)
 ON CONFLICT (user_id) DO NOTHING;
 
 -- EXPENSE
 INSERT INTO expense (user_id, total_amount, remaining_amount)
 VALUES
   (2, 25000, 25000),
-  (3, 50000, 50000)
+  (1, 50000, 50000)
 ON CONFLICT (user_id) DO NOTHING;
 
 -- DISCOUNT
 INSERT INTO discount (user_id, total_discount, remaining_discount)
 VALUES
   (2, 30, 30),
-  (3 ,30, 30)
+  (1, 40, 40)
 ON CONFLICT (user_id) DO NOTHING;
 
 COMMIT;
